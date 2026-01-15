1fefe602ded72defc453aaba5eef0fda5d6f330d
IzgoyObshchestva <zxcqwe@mail.ru>
2026-01-15 16:07:05 +0700

    ✔️ Добавление [v0.12.0]: подключение маршрутов API
    Были подключены основные маршрут в API (заранее)

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,9 @@ from contextlib import  asynccontextmanager
 from .db.init_db import init_db
 from .db.session import engine
 
+from .api.v1.user import router as user_router_v1
+from .api.v1.game import router as game_router_v1
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await init_db()
@@ -15,6 +18,9 @@ async def lifespan(app: FastAPI):
         await engine.dispose()
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(user_router_v1, prefix='/api/v1', tags=['User'])
+app.include_router(game_router_v1, prefix='/api/v1', tags=['Game'])
 
 @app.get('/')
 async def zxc():
