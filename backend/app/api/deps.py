1b45e1c617a6004709277b3ddd80d801d98b8ce8
IzgoyObshchestva <zxcqwe@mail.ru>
2026-01-17 03:06:28 +0700

    ✔️ Добавление [v0.17.0]: get_user_id_in_db
    Добавлена функция "get_user_id_in_db" для того, чтобы вынести функционал поиска пользователя по его telegram id в отельный Depends, а потом использовать его в роутерах Game

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -1,1 +1,18 @@
 from ..services.auth import verify_bot_secret
+
+from typing import Annotated
+from fastapi import Query, Depends
+from ..db.session import get_db
+from sqlalchemy.ext.asyncio import AsyncSession
+from ..crud.user import UserRepository
+
+async def get_user_id_in_db(
+    user_telegtam_id: Annotated[str, Query()],
+    db: AsyncSession = Depends(get_db)
+):
+    '''
+    Получение id пользователя из БД по его telegram id
+    '''
+    repo = UserRepository(db)
+    your_id = await repo.get_user_by_tg_id(user_telegtam_id)
+    return your_id.id
